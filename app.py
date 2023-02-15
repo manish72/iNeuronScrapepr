78be4b43a41a53a82a8b681fbe72805d514f3c7c
manish72 <manishguru72@gmail.com>
2023-02-15 15:38:56 +0530

    PDF usecase completed

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request,jsonify
+from flask import Flask, render_template, request, send_from_directory
 from flask_cors import CORS,cross_origin
+from flask import make_response
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
@@ -23,35 +24,35 @@ def fetchCourseList():
     scrapper = ineuron.iNeuronReviewScrapper()  
     return render_template("courseslist.html",result=scrapper.fetch_courses_by_subCategory(subID))
 
-@app.route("/course", methods = ['GET'])
+@app.route("/course", methods = ['GET','POST'])
 def fetchCourse():
     course = request.args['coursename']
     scrapper1 = ineuron.iNeuronReviewScrapper()
     courseDetails = scrapper1.scrap_one_courseInfo(course)
-    #print(courseDetails)
-    #return render_template("coursedetails.html",result=courseDetails)
-
-    # Get the HTML output
-    out = render_template("coursedetails.html",result=courseDetails)
-    #print(out)
+    html = render_template("coursedetails.html", result=courseDetails)
     # PDF options
     options = {
-        "orientation": "landscape",
+        "orientation": "portrait",
         "page-size": "A4",
         "margin-top": "1.0cm",
         "margin-right": "1.0cm",
         "margin-bottom": "1.0cm",
         "margin-left": "1.0cm",
         "encoding": "UTF-8",
+        "enable-local-file-access": ""
     }
-    
-    # Build PDF from HTML
-    config = pdfkit.configuration(wkhtmltopdf='/opt/bin/wkhtmltopdf')
-    pdf = pdfkit.from_string(out, options=options, configuration=config, verbose=True)
-    print(pdf)
-
-    # Download the PDF
-    return Response(pdf, mimetype="application/pdf")
+    # wkhtmltopdf windows installation location ---> C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe
+    config = pdfkit.configuration(wkhtmltopdf = r"wkhtmltopdf.exe")
+    pdf = ""
+    response = ""
+    try:
+        pdf = pdfkit.from_string(html,'PDFs/'+course+'.pdf',options=options,css='static/css/style.css', configuration=config)
+        #response = make_response(pdf)
+        #response.headers['Content-Type']='application/pdf'
+    except Exception as e :
+        logging.info(e)
+    return send_from_directory('PDFs/',course+'.pdf', as_attachment=True) 
+    #return render_template("coursedetails.html",result=courseDetails)
     
 
 if __name__=="__main__":
